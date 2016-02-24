47c239a4bde2640ad4cb673395dff872d3ab0e2e
Dustin Van Tate Testa <toast27@gmail.com>
2016-02-24 10:18:08 -0500

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -1,41 +1,40 @@
 #include "Robot.h"
 
-// some important constants...
-#define STOPPING_DISTANCE_INCHES 36.0 // 3 feet
-
 Robot::Robot() : //inline initializations:
-	myRobot(0, 1), //left0, right1
-	gearShifter(0,1), shooterPiston(2,3), //solenoids
-	driveCtl(0), shootCtl(1), //they want 2 joysicks now
-	airPump(0), //compressor
-	shooterElevator(2), //set elevation of the shooter
-	inAndOut1(8), inAndOut2(9), //shooter motors
-	sonar(2, 3), //ultrasonic range-finder
-	/// they want potentiometers instead of limit switches :(
-	shooterArmPot(0/*Analog input port number*/, 90/*degrees of rotation*/, 0/*initial rotation*/),
-	accel(Accelerometer::kRange_4G) // the accelerometer in the roboRIO (not used...)
+    myRobot(0, 1), //left0, right1
+    gearShifter(0,1), shooterPiston(2,3), //solenoids
+    stick(0), //joystick controlled robot
+    airPump(0), //compressor
+    shooterElevator(2), //set elevation of the shooter
+    inAndOut1(8), inAndOut2(9), //shooter motors
+	shooterUpLim(0), shooterDownLim(1), shooterInLim(5) //limit switches
 {
 	myRobot.SetExpiration(0.1);
 }
 
 
-void Robot::RobotInit(){
-	//setup the auto-chooser:
-	//chooser->AddDefault(autoStopAtObstacle, (void*)&autoStopAtObstacle);
-	//chooser->AddObject(autoLowBar, (void*)&autoLowBar);
-	//chooser->AddObject(autoSeeSaws, (void*)&autoSeeSaws);
-
-	//SmartDashboard::PutData("Auto Modes", chooser);
-
-	//get camera feed and post it to the smartdashboard
-	CameraServer::GetInstance()->SetQuality(50);
-	CameraServer::GetInstance()->StartAutomaticCapture("cam0");// camera name in the web interface
-
-	std::cout <<"Hello world!" <<std::endl; // :P
-
+//used to control a motors direction using 2 buttons (fwd & bkwd)
+// function template that allows control of any motor controller using 2 buttons.
+//ToDo: (in the future add a condition param)
+template <class MOTCTLR>
+void Robot::setMotorDirection(MOTCTLR& motor, Joystick& joystick, const uint8_t& fwd, const uint8_t& bkwd){
+	if (joystick.GetRawButton(fwd) == joystick.GetRawButton(bkwd)) motor.SetSpeed(0);
+	else if (joystick.GetRawButton(fwd)) motor.SetSpeed(1);
+	else motor.SetSpeed(-1); //bkwd
 }
 
+void Robot::RobotInit(){
+	//setup the auto-chooser:
+	chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
+	chooser->AddObject(autoNameCustom1, (void*)&autoNameCustom1);
 
+	SmartDashboard::PutData("Auto Modes", chooser);
+
+	//get camera feed and post it to the smartdashboard (*NOTE: disabled until we get a camera)
+	//CameraServer::GetInstance()->SetQuality(50);
+	//CameraServer::GetInstance()->StartAutomaticCapture("cam0");//get camera name (cam0) from the roborio web interface
+
+}
 /**Notes on autonomous chooing code:
  * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
  * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
@@ -46,184 +45,106 @@ void Robot::RobotInit(){
  * If using the SendableChooser make sure to add them to the chooser code above as well.
  */
 void Robot::AutonomousInit(){
-
-	// start filling the tank
+	//start filling the tank
 	airPump.SetClosedLoopControl(true);
 
-    // auto-choosing code
-	//autoSelected = *((std::string*)chooser->GetSelected()); //4 C++/Java smartdashboard
-	std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoStopAtObstacle); //use this for labview drive station
+    //auto-choosing code
+	autoSelected = *((std::string*)chooser->GetSelected());
+	//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault); //use this for labview drive station
 
-	// print the chosen autonomous code to the console
-	std::cout <<"Autonomous has begun!" <<std::endl
-			<<"Auto selected: " <<autoSelected <<std::endl;
+	//print the chosen autonomous code to the terminal
+	std::cout <<"Autonomous has started...\nAuto selected: " <<autoSelected <<"\n";
 
-
-	// not quite sure what this does... but it's needed.
-	sonar.SetAutomaticMode(true); // turns on automatic mode
-
-	// disable safety on drive-train
-	myRobot.SetSafetyEnabled(false);
-
-	if (autoSelected == autoLowBar) {
-		// drive until the low bar flap thing
-		while (sonar.GetRangeInches() < STOPPING_DISTANCE_INCHES)
-			myRobot.Drive(-0.5f, 0);
-
-		// drive through the low-bar
-		myRobot.ArcadeDrive(0, -0.25f);
-		Wait(3);
-
-		// drive until the wall on the other side
-		while (sonar.GetRangeInches() < STOPPING_DISTANCE_INCHES)
-			myRobot.Drive(-0.5f, 0);
-
-		// turn approximatly 90 degrees right. (towards goal)
-		myRobot.ArcadeDrive(0, -0.2f);
-		Wait(0.5);	//adjust wait period
-
-		// start driving forward again
-		myRobot.ArcadeDrive(0.5f, 0.5f);
-
-		// start spinning the shooter
-		inAndOut1.SetSpeed(1);
-		inAndOut2.SetSpeed(1);
-		//Wait(0.75);
-
-		// drive up to the goal
-		while (sonar.GetRangeInches() < STOPPING_DISTANCE_INCHES)
-			myRobot.Drive(-0.5f, 0);
-
-		// shoot the ball [into the goal].
-		shooterPiston.Set(DoubleSolenoid::Value::kReverse);
-
-		// stop spinning the shooter
-		inAndOut1.SetSpeed(0);
-		inAndOut2.SetSpeed(0);
-
-		// stop moving (wait until tele-op starts)
-		myRobot.ArcadeDrive(0.0f, 0.0f);
-
-	} else if (autoSelected == autoSeeSaws) {
-		// autonomous code to go over the see-saws
-
-		///TODO:
-		/// drive up to the see-saws
-		/// use the shooter to lower the see-saws (might not be possible)
-		/// drive over the see-saws
-		/// proceed to emulate the low-bar autonomous code.
-
-	} else { //default autonomous code
+	//run selected autonomous code
+	if (autoSelected == autoNameCustom1) {
+		//Custom Auto goes here
+	} else {
 		//Default Auto goes here
 	}
 }
 
 void Robot::AutonomousPeriodic(){
-	if (autoSelected == autoLowBar) {
-		//autonomous code to go through the low bar
-	} else if (autoSelected == autoSeeSaws) {
-		//autonomous code to go over the see-saws
-	} else {//default autonomous code
-		//drive forward and stop 3 feet in front of the vertical obstacle.
-		//the robot and field are built in inches, so it's probably best not to use metric =(
-		if (sonar.GetRangeInches() > STOPPING_DISTANCE_INCHES)
-			myRobot.Drive(-0.3f, 0);
-		else myRobot.Drive(-0.5f, 0);
+	if (autoSelected == autoNameCustom1) {
+		//Custom Auto goes here
+	} else {
+		//Default Auto goes here
+
+		///The simplest (lame) autonomous code
+		// if (NO_OBSTACLES_IN_THE_WAY) DRIVE FORWARD
+		// else STOP
 	}
 }
-
 
 void Robot::TeleopInit(){
 	//start filling the tank
 	airPump.SetClosedLoopControl(true);
 
 	//tele-op has started.
-	std::cout <<"Tele-op has started - Awaiting your command\n";
+	std::cout <<"Teleop has started...\n";
 
-	//start out in low gear
-	gearShifter.Set(DoubleSolenoid::Value::kForward);
+	//start out in low gear (50% CHANCE OF BEING CORRECT)
+	gearShifter.Set(DoubleSolenoid::Value::kReverse);
 
 }
 
 void Robot::TeleopPeriodic(){
+
+	//"all of these could be parallel processes."
+	// -LabVIEW
+
 	//drive the robot
-	// removeGhost() eliminates input less than 15% so that the robot won't move by itself
-	myRobot.ArcadeDrive(
-		-utils::removeGhost(driveCtl.GetRawAxis(1)),
-		-utils::removeGhost(driveCtl.GetRawAxis(0))
-	);
+	myRobot.ArcadeDrive( -stick.GetY(), -stick.GetX(), false);
 
-	//shift gears a==low b==high
-	if (driveCtl.GetRawButton(1) && m_isHighGear) { // a
+	//shift gears as according to buttons 11 & 12 (adjust upon request)
+	if (stick.GetRawButton(11) && m_isHighGear) {
 		gearShifter.Set(DoubleSolenoid::Value::kForward);
-		std::cout <<"Low Gear" <<std::endl;
+		std::cout <<"Low Gear\n";
 		m_isHighGear = !m_isHighGear;
-	} else if (driveCtl.GetRawButton(2) && !m_isHighGear) { // b
+	} else if (stick.GetRawButton(12) && !m_isHighGear) {
 		gearShifter.Set(DoubleSolenoid::Value::kReverse);
-		std::cout <<"High Gear" <<std::endl;
+		std::cout <<"High Gear\n";
 		m_isHighGear = !m_isHighGear;
 	}
 
-	// adjust shooter's vertical elevation using the D-pad
-	if (shootCtl.GetPOV() > 90 && shootCtl.GetPOV() < 270 && shooterArmPot.Get() <= 75) // D-pad down
-		shooterElevator.SetSpeed(-0.5f);
-	else if(shootCtl.GetPOV() < 90 && shootCtl.GetPOV() > 270 && shooterArmPot.Get() >= 0) // D-pad up
-		shooterElevator.SetSpeed(-0.5f);
-	else shooterElevator.SetSpeed(0);
-
-
-	//intake and pre-fire controls
-	if (shootCtl.GetRawAxis(3) && !shootCtl.GetRawAxis(2)){ // pre-shoot
-		inAndOut1.SetSpeed(1); // was getting some problems with the motor stalling out
-		inAndOut2.SetSpeed(1);
-	} else if (shootCtl.GetRawAxis(2) && !shootCtl.GetRawAxis(3)){ // intake
-		inAndOut1.SetSpeed(-0.75f);
-		inAndOut2.SetSpeed(-0.75f);
-	} else {
-		inAndOut1.SetSpeed(0);
-		inAndOut2.SetSpeed(0);
-	}
-
-	// We're using a piston for the shooter :P
-	if (shootCtl.GetRawAxis(3) > 0.95f) // fire the shooter
+	//sebastian wants to test using a piston for the shooter
+	if (stick.GetTrigger() && !stick.GetRawButton(2)) // fire the shooter
 		shooterPiston.Set(DoubleSolenoid::Value::kReverse);
-	else shooterPiston.Set(DoubleSolenoid::Value::kForward); // retract the shooter
+	else if (stick.GetTrigger() && stick.GetRawButton(2))
+		std::cout <<"Warning: you cannot shoot and suck at the same time.\n";
+	else shooterPiston.Set(DoubleSolenoid::Value::kForward);
 
-	// rumble both controllers when firing and when switching gears.
-	if (shootCtl.GetRawAxis(3) > 0.95f) { // rumble for fire (kobe)
-		driveCtl.SetRumble(driveCtl.kLeftRumble, 1);
-		driveCtl.SetRumble(driveCtl.kRightRumble, 1);
-		shootCtl.SetRumble(shootCtl.kLeftRumble, 1);
-		shootCtl.SetRumble(shootCtl.kRightRumble, 1);
-	} else if (driveCtl.GetRawButton(1) || driveCtl.GetRawButton(2)) { // rumble for gear-shift
-		driveCtl.SetRumble(driveCtl.kLeftRumble, 1);
-		driveCtl.SetRumble(driveCtl.kRightRumble, 1);
-		shootCtl.SetRumble(shootCtl.kLeftRumble, 1);
-		shootCtl.SetRumble(shootCtl.kRightRumble, 1);
-	} else { // no rumble
-		driveCtl.SetRumble(driveCtl.kLeftRumble, 0);
-		driveCtl.SetRumble(driveCtl.kRightRumble, 0);
-		shootCtl.SetRumble(shootCtl.kLeftRumble, 0);
-		shootCtl.SetRumble(shootCtl.kRightRumble, 0);
-	}
+	//adjust shooter's vertical angle
+	/// the limits will likely have to be swapped (50% chance)
+	// if it's safe to move the motor, run the code to do so
+	if ((stick.GetRawButton(4) && shooterUpLim.Get()) != (stick.GetRawButton(6) && shooterDownLim.Get()))
+		setMotorDirection(shooterElevator, stick, 4, 6);
+	else //stop the motor if it isn't safe
+		shooterElevator.SetSpeed(0);
+
+	//intake and pre-fire controls (button 3 starts the shooter motors spinning)
+	if ((stick.GetRawButton(2) && shooterInLim.Get()) || stick.GetRawButton(3))
+		setMotorDirection(inAndOut1, stick, 3, 2); //set intake/fire
+	else inAndOut1.SetSpeed(0);
+
+    //this is because they want the wire colors to match on the motors
+	if ((stick.GetRawButton(3) && shooterInLim.Get()) || stick.GetRawButton(2))
+		setMotorDirection(inAndOut2, stick, 2, 3); //set intake/fire
+	else inAndOut2.SetSpeed(0);
 
 	//print "Kobe!!" to the terminal when we shoot (for good luck)
-	if (m_kobe && shootCtl.GetRawAxis(3) > 0.95f) {
-		std::cout <<"Kobe!!" <<std::endl;
+	/// this code makes it only print once for each time the trigger is pressed
+	if (m_kobe && stick.GetTrigger()) {
+		std::cout <<"Kobe!!\n";
 		m_kobe = false;
-	} else if (shootCtl.GetRawAxis(3) <= 0.95f) {
+	} else if (!stick.GetTrigger()) {
 		m_kobe = true;
 	}
-
-	//print the currrent range to the console
-	std::cout <<"\rUltrasonic Range: " <<sonar.GetRangeInches() <<"inches";
 }
 
 void Robot::TestInit(){
-	std::cout <<"Testing mode enabled...\nCurrently doing: {NULL}" <<std::endl;
+	std::cout <<"Testing mode enabled...\nCurrently doing: (NULL)"; //as though there was a purpose for testing mode...
 }
 
-void Robot::TestPeriodic()
-	{ lw->Run(); }
+void Robot::TestPeriodic(){	lw->Run(); }
 
 START_ROBOT_CLASS(Robot)
